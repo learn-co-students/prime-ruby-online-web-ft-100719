a60433a8612bf970c35b90281f4e0619ef1b2d35
Gina Sekhar <ginasekhar@yahoo.com>
2019-10-09 13:02:09 +0000

    Done.

--- prime.rb
+++ prime.rb
@@ -1,1 +1,18 @@
 # Add  code here!
+def prime?(num)
+  if num <= 0 
+    return FALSE
+  elsif num == 1
+    return TRUE
+  end
+  
+  root = sqrt(num)
+  
+  (2..root).each do |divisor|
+    if num % divisor == 0 
+      return FALSE
+    end
+  end
+  return TRUE
+  
+end
