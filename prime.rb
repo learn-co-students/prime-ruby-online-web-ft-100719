772928518c5bdd4724bbdc6a923fc0e3a4e364c8
Gina Sekhar <ginasekhar@yahoo.com>
2019-10-09 16:43:41 +0000

    Done.

--- prime.rb
+++ prime.rb
@@ -2,25 +2,27 @@
 require 'pry'
 def prime?(num)
   
-  if num <= 0 
+  if num <= 1  
     puts "#{num} les than 0"
     return FALSE
-  elsif (num == 1 || num == 2)
+  elsif (num == 2)
     puts "#{num} l or 2"
     return TRUE
-  end
+  end #if
 
 
  divisor_array = Array(3..num/2)
  divisor_array.delete_if {|divisor| divisor.even? } 
  divisor_array.unshift(2)
+ 
  #binding.pry
  divisor_array.each do |divisor|
     if (num != divisor) && (num % divisor == 0)
       puts "#{num} #{divisor} false"
       return FALSE
     end
-  end
+  end #each
+  
   puts "#{num} true"
   return TRUE
   
