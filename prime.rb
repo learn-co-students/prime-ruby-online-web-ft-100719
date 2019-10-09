246542a6a4019dbbddbf801e2f3b322d08ef39ba
Kathleen Kamali <kathleenkamali@gmail.com>
2019-10-09 18:45:59 +0000

    Done.

--- prime.rb
+++ prime.rb
@@ -1,1 +1,13 @@
-# Add  code here!
+def prime?(num)
+  if num <= 1 
+    false 
+  elsif num == 2 
+    true 
+  elsif num > 1 
+    test_numbers = (2...num)
+    test_numbers.each do |test|
+      return false if num % test == 0 
+    end
+    true 
+  end 
+end
