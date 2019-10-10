24e928bf428ebc645b17f5b991e4ff349d881bf8
Geoff Elliott <tinywv@gmail.com>
2019-10-10 19:18:26 +0000

    Done.

--- prime.rb
+++ prime.rb
@@ -1,1 +1,12 @@
-# Add  code here!
+def prime?(num)
+first_num = 2
+  if num > 1
+    num_range = (first_num..num - 1).to_a
+      num_range.none? do |num_test|
+        num % num_test == 0
+      end
+    else
+      false
+    end
+
+end
